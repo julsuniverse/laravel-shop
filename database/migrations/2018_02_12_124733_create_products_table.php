d93d86236061ea3bbb31f0ed1729ed68cfa126cc
Julia <julsuniverse@gmail.com>
2018-02-12 17:04:57 +0200

    products and catalog  migrations

---
+++ database/migrations/2018_02_12_124733_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->double('price');
+            $table->string('alias', 255);
+            $table->string('photo', 255);
+            $table->integer('shop_id');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
